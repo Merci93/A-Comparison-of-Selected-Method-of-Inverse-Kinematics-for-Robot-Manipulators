4b47ce02137bfe0731f1eabc385c465c1d98463f
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-07-17 02:17:08 +0200

    print 4-DOF results

---
+++ print_4DOF.m
@@ -0,0 +1,9 @@
+function [] = print_4DOF(i, dist, kinematics_, config)
+
+fprintf('Number of iterations = %d\n',i);
+fprintf('Distance between current position and goal = %f\n',dist)
+fprintf('kinematics = [%f , %f]\n', kinematics_(1), kinematics_(2));
+fprintf('Configurations (q) = [%f %f %f %f]\n\n',config(1,i),config(2,i),config(3,i),config(4,i));
+
+
+end
